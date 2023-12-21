c41444460c5baa8a7114bc951d68f9186349b529
lsantarcangelo <lsantarcangelo@yahoo.com.ar>
2023-12-20 21:37:07 -0300

    Se agregan funcionalidades para pausar y reactivar suscripciones

--- controllers/mercadoApiController.js
+++ controllers/mercadoApiController.js
@@ -21,30 +21,35 @@ const mercadoApiController = {
         const loggedUser = req.session.loggedUser;
         console.log(body.payer_email);
         const localSuscription = await db.Suscription.findOne({where: {user_id: loggedUser.id}});
-        if (!localSuscription) {    
-            const suscription = await fetch( url, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
-                },
-                body: JSON.stringify(body)
-            }).then(response => response.json());
-            console.log(suscription);
-            await db.Suscription.create({
-                'suscription_id': suscription.id,
-                'user_id': req.session.loggedUser.id,
-                'payer_id': suscription.payer_id,
-                'payer_email': suscription.payer_email,
-                'application_id': suscription.application_id,
-                'status': suscription.status,
-                'reason': suscription.reason,
-                'date_created': suscription.date_created,
-                'currency_id': suscription.auto_recurring.currency_id,
-                'frequency_type': suscription.auto_recurring.frequency_type,
-                'transaction_amount': suscription.auto_recurring.transaction_amount 
-            })
-            res.redirect(suscription.init_point);
+        if (!localSuscription) {
+            try {
+                const suscription = await fetch( url, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+                    },
+                    body: JSON.stringify(body)
+                })
+                .then(response => response.json());
+                console.log(suscription);
+                await db.Suscription.create({
+                    'suscription_id': suscription.id,
+                    'user_id': req.session.loggedUser.id,
+                    'payer_id': suscription.payer_id,
+                    'payer_email': suscription.payer_email,
+                    'application_id': suscription.application_id,
+                    'status': suscription.status,
+                    'reason': suscription.reason,
+                    'date_created': suscription.date_created,
+                    'currency_id': suscription.auto_recurring.currency_id,
+                    'frequency_type': suscription.auto_recurring.frequency_type,
+                    'transaction_amount': suscription.auto_recurring.transaction_amount 
+                })
+                res.redirect(suscription.init_point);                
+            } catch (error) {
+                res.send(error.message);   
+            }
         } else {
             res.send('Ya posee una suscripcion activa')
         }                
@@ -70,6 +75,49 @@ const mercadoApiController = {
         } else {
             res.send('No posee ninguna suscripcion')
         }
+    },
+    pauseSuscription: async function (req, res) {
+        const loggedUser = req.session.loggedUser;
+        const localSuscription = await db.Suscription.findOne({where: {user_id: loggedUser.id}});
+        const preId = localSuscription.dataValues.suscription_id;
+        const url = `https://api.mercadopago.com/preapproval/${preId}`;
+        const body = {
+            reason: "LeyesApp",
+            back_url: "https://www.mercadopago.com.ar/",
+            status: "paused"
+        };
+        let suscriptionUpdate = await fetch( url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
+            },
+            body: JSON.stringify(body)
+        })
+        .then(response => response.json());
+        console.log(suscriptionUpdate);
+        res.redirect('/users/profile/');  
+    },
+    reactivateSuscription: async function (req, res) {
+        const loggedUser = req.session.loggedUser;
+        const localSuscription = await db.Suscription.findOne({ where: {user_id: loggedUser.id} });
+        const preId = localSuscription.dataValues.suscription_id;
+        const url = `https://api.mercadopago.com/preapproval/${preId}`;
+        const body = {
+            reason: "LeyesApp",
+            back_url: "https://www.mercadopago.com.ar/",
+            status: "authorized"
+        };
+        let suscriptionUpdate = await fetch( url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`
+            },
+            body: JSON.stringify(body)
+        })
+        .then(response => response.json());
+        res.redirect('/api/suscription');
     }  
 }
 
